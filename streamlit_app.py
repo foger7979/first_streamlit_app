36b0f8249df15d2213479e1af87c2e74a8003054
foger7979 <103907006+foger7979@users.noreply.github.com>
2022-08-20 08:01:21 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 streamlit.button('x')
 streamlit.slider('y')
-streamlit.header("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.body("Omega 3 & Blueberry Oatmeal")
