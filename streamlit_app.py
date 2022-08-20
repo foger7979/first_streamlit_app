ea3222fecf640b2f20bf3fc4e6461b72dbdd490d
foger7979 <103907006+foger7979@users.noreply.github.com>
2022-08-20 08:08:57 -0800

    Update streamlit_app.py
    Trying to comment out lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit
 
 streamlit.title("My Mom's New Healthy Diner")
-//streamlit.button('x')
-//streamlit.slider('y')
-//streamlit.write("testing out the streamlit library")
-//streamlit.text("")
-//streamlit.text("")
+--streamlit.button('x')
+--streamlit.slider('y')
+--streamlit.write("testing out the streamlit library")
+--streamlit.text("")
+--streamlit.text("")
 streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
